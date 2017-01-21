957b803fb509470716fc78259d7ff8bce1e297b7
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-01-21 06:13:28 +0100

    Create a punch-dashes-markdown-parser test

---
+++ test/punch-dashes-markdown-parser_test.js
@@ -0,0 +1,25 @@
+var assert = require('chai').assert;
+var parser = require('../src/punch-dashes-markdown-parser.js');
+
+describe('parser', function(){
+    it('should parse markdown', function(done){
+        parser.parse([
+            '# Hello, World!'
+        ].join('\n'), function(err, output){
+            assert.deepEqual(output, '<section><h1 id="hello-world-">Hello, World!</h1>\n</section>');
+            done();
+        });
+
+    });
+
+    it('should parse markdown separated by dashes', function(done){
+        parser.parse([
+            '# Hello, World!',
+            "---",
+            "# Goodbye, World!"
+        ].join('\n'), function(err, output){
+            assert.deepEqual(output, '<section><h1 id="hello-world-">Hello, World!</h1>\n</section><section><h1 id="goodbye-world-">Goodbye, World!</h1>\n</section>');
+            done();
+        });
+    });
+});
