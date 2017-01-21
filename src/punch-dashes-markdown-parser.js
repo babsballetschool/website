2285ab7a57f36a2ebf899e129dd74122866ffe75
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-01-21 06:03:41 +0100

    Remove logging from script

--- src/punch-dashes-markdown-parser.js
+++ src/punch-dashes-markdown-parser.js
@@ -3,7 +3,6 @@ var marked = require('marked');
 function parse(input, callback) {
     var output, err;
     try {
-        console.log(marked(input));
         output = input.split(/---/g)
             .map(function(md){
                 return marked(md);
@@ -11,10 +10,8 @@ function parse(input, callback) {
             .map(function(story){
                 return '<section>' + story + '</section>';
             }).join('');
-        console.log(output);
     } catch(e) {
         err = e;
-        console.log(err);
     }
     return callback(err, output);
 }
