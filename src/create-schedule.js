eb6f03d3121ba97f08ca2d9d829a711167c66078
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-02-21 10:32:19 +0100

    Create a row of headers

--- src/create-schedule.js
+++ src/create-schedule.js
@@ -5,9 +5,26 @@ const PDFDocument = require('pdfkit');
 var doc = new PDFDocument();
 doc.pipe(fs.createWriteStream('templates/resources/pdf/schedule.pdf'));
 
-doc
-    .moveTo(0,0)
-    .lineTo(100, 100)
-    .stroke();
+const font = 'Times-Roman';
+const font_size = 8;
+
+doc.font(font);
+doc.fontSize(font_size);
+
+const cell_width = 50;
+const header_height = 10;
+const padding_width = 5;
+const padding_height = 3;
+
+["Tijd", "Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag"]
+    .forEach(function(header, index){
+        var x = padding_width + index * (cell_width + padding_width);
+        var y = padding_height;
+        doc.rect(x, y, cell_width, header_height);
+        var text_y = y + header_height/2 - font_size/3;
+        doc.text(header, x, text_y, { align: 'center', width: cell_width });
+    });
+
+doc.stroke();
 
 doc.end();
